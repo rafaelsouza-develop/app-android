0d690b5f607bcc7e154d19e67fc9ea8f392355d7
Rafael Souza <rafaelsouza.develop@gmail.com>
2021-01-08 08:33:55 -0300

    feat: add RegisterViewModel to ViewModelModule

--- app/src/main/java/br/com/app4pets/app/di/ViewModelModule.kt
+++ app/src/main/java/br/com/app4pets/app/di/ViewModelModule.kt
@@ -1,11 +1,13 @@
 package br.com.app4pets.app.di
 
 import br.com.app4pets.app.modules.login.LoginViewModel
+import br.com.app4pets.app.modules.register.RegisterViewModel
 import br.com.app4pets.app.util.DispatcherProvider
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
     factory { DispatcherProvider() }
-    factory { LoginViewModel(get(), get()) }
+    factory { LoginViewModel(get(), get(), get()) }
+    factory { RegisterViewModel(get(), get()) }
 }
